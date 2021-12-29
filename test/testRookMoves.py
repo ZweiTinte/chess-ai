749bd6f5d7cf846974fa7aa0423dd4ad3792b627
ZweiTinte <zweitinte@gmail.com>
2021-12-29 18:22:03 +0100

    rook moves tests multiple units

--- test/testRookMoves.py
+++ test/testRookMoves.py
@@ -47,3 +47,67 @@ class TestRookMoves(unittest.TestCase):
         expectedMoves = ["35", "36", "37", "33", "32", "31", "30", "04", "14", "24", "44", "54", "64", "74"]
         assertExpectedMovesResults(expectedMoves, self.rookUnit)
         assertNumberOfWhiteAndTotalUnits(self.game, 1, 1)
+
+    # --- OPPONENT UNITS ---
+
+    def testMidGamePositionSurroundedByOpponentRookMoves(self):
+        self.game.setUnitOnBoard(3, 4, self.rookUnit)
+        self.game.setUnitOnBoard(3, 5, Unit(ROOK, self.game.black))
+        self.game.setUnitOnBoard(3, 3, Unit(ROOK, self.game.black))
+        self.game.setUnitOnBoard(4, 4, Unit(ROOK, self.game.black))
+        self.game.setUnitOnBoard(2, 4, Unit(ROOK, self.game.black))
+        calculatePossibleMoves(self.game, self.game.turn, True)
+        expectedMoves = ["35", "33", "44", "24"]
+        assertExpectedMovesResults(expectedMoves, self.rookUnit)
+        assertNumberOfWhiteAndTotalUnits(self.game, 1, 5)
+
+    def testMidGamePositionFarSurroundedByOpponentRookMoves(self):
+        self.game.setUnitOnBoard(3, 4, self.rookUnit)
+        self.game.setUnitOnBoard(0, 4, Unit(ROOK, self.game.black))
+        self.game.setUnitOnBoard(7, 4, Unit(ROOK, self.game.black))
+        self.game.setUnitOnBoard(3, 0, Unit(ROOK, self.game.black))
+        self.game.setUnitOnBoard(3, 7, Unit(ROOK, self.game.black))
+        calculatePossibleMoves(self.game, self.game.turn, True)
+        expectedMoves = ["35", "36", "37", "33", "32", "31", "30", "04", "14", "24", "44", "54", "64", "74"]
+        assertExpectedMovesResults(expectedMoves, self.rookUnit)
+        assertNumberOfWhiteAndTotalUnits(self.game, 1, 5)
+
+    # --- ALLY UNITS ---
+
+    def testMidGamePositionSurroundedByAllyRookMoves(self):
+        self.game.setUnitOnBoard(3, 4, self.rookUnit)
+        self.game.setUnitOnBoard(3, 5, Unit(ROOK, self.game.white))
+        self.game.setUnitOnBoard(3, 3, Unit(ROOK, self.game.white))
+        self.game.setUnitOnBoard(4, 4, Unit(ROOK, self.game.white))
+        self.game.setUnitOnBoard(2, 4, Unit(ROOK, self.game.white))
+        calculatePossibleMoves(self.game, self.game.turn, True)
+        assertExpectedMovesResults([], self.rookUnit)
+        assertNumberOfWhiteAndTotalUnits(self.game, 5, 5)
+
+    def testMidGamePositionFarSurroundedByAllyRookMoves(self):
+        self.game.setUnitOnBoard(3, 4, self.rookUnit)
+        self.game.setUnitOnBoard(0, 4, Unit(ROOK, self.game.white))
+        self.game.setUnitOnBoard(7, 4, Unit(ROOK, self.game.white))
+        self.game.setUnitOnBoard(3, 0, Unit(ROOK, self.game.white))
+        self.game.setUnitOnBoard(3, 7, Unit(ROOK, self.game.white))
+        calculatePossibleMoves(self.game, self.game.turn, True)
+        expectedMoves = ["35", "36", "33", "32", "31", "14", "24", "44", "54", "64"]
+        assertExpectedMovesResults(expectedMoves, self.rookUnit)
+        assertNumberOfWhiteAndTotalUnits(self.game, 5, 5)
+
+    # --- MIXED ROWS AND COLS ---
+
+    def testMidGamePositionFarSurroundedByMultipleUnitsRookMoves(self):
+        self.game.setUnitOnBoard(3, 4, self.rookUnit)
+        self.game.setUnitOnBoard(0, 4, Unit(ROOK, self.game.white))
+        self.game.setUnitOnBoard(1, 4, Unit(ROOK, self.game.black))
+        self.game.setUnitOnBoard(7, 4, Unit(ROOK, self.game.black))
+        self.game.setUnitOnBoard(6, 4, Unit(ROOK, self.game.white))
+        self.game.setUnitOnBoard(3, 0, Unit(ROOK, self.game.white))
+        self.game.setUnitOnBoard(3, 1, Unit(ROOK, self.game.black))
+        self.game.setUnitOnBoard(3, 7, Unit(ROOK, self.game.black))
+        self.game.setUnitOnBoard(3, 6, Unit(ROOK, self.game.white))
+        calculatePossibleMoves(self.game, self.game.turn, True)
+        expectedMoves = ["35", "33", "32", "31", "14", "24", "44", "54"]
+        assertExpectedMovesResults(expectedMoves, self.rookUnit)
+        assertNumberOfWhiteAndTotalUnits(self.game, 5, 9)
