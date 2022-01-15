89fd88d183c113fe04f3ac927ea7dc0b0fe6fb49
ZweiTinte <zweitinte@gmail.com>
2022-01-15 22:08:54 +0100

    added more knight moves tests

--- test/testKnightMoves.py
+++ test/testKnightMoves.py
@@ -48,6 +48,89 @@ class TestKnightMoves(unittest.TestCase):
         assertExpectedMovesResults(expectedMoves, self.knightUnit)
         assertNumberOfWhiteAndTotalUnits(self.game, 1, 1)
 
+    def testUpperLeftBorderKnightMoves(self):
+        self.game.setUnitOnBoard(0, 6, self.knightUnit)
+        calculatePossibleMoves(self.game, self.game.turn, True)
+        expectedMoves = ["27", "14", "25"]
+        assertExpectedMovesResults(expectedMoves, self.knightUnit)
+        assertNumberOfWhiteAndTotalUnits(self.game, 1, 1)
+
+    def testLowerLeftBorderKnightMoves(self):
+        self.game.setUnitOnBoard(0, 1, self.knightUnit)
+        calculatePossibleMoves(self.game, self.game.turn, True)
+        expectedMoves = ["20", "13", "22"]
+        assertExpectedMovesResults(expectedMoves, self.knightUnit)
+        assertNumberOfWhiteAndTotalUnits(self.game, 1, 1)
+
+    def testUpperRightBorderKnightMoves(self):
+        self.game.setUnitOnBoard(7, 6, self.knightUnit)
+        calculatePossibleMoves(self.game, self.game.turn, True)
+        expectedMoves = ["57", "64", "55"]
+        assertExpectedMovesResults(expectedMoves, self.knightUnit)
+        assertNumberOfWhiteAndTotalUnits(self.game, 1, 1)
+
+    def testLowerRightBorderKnightMoves(self):
+        self.game.setUnitOnBoard(7, 1, self.knightUnit)
+        calculatePossibleMoves(self.game, self.game.turn, True)
+        expectedMoves = ["50", "63", "52"]
+        assertExpectedMovesResults(expectedMoves, self.knightUnit)
+        assertNumberOfWhiteAndTotalUnits(self.game, 1, 1)
+
+    def testUpperLeftCornerKnightMoves(self):
+        self.game.setUnitOnBoard(0, 7, self.knightUnit)
+        calculatePossibleMoves(self.game, self.game.turn, True)
+        expectedMoves = ["15", "26"]
+        assertExpectedMovesResults(expectedMoves, self.knightUnit)
+        assertNumberOfWhiteAndTotalUnits(self.game, 1, 1)
+
+    def testLowerLeftCornerKnightMoves(self):
+        self.game.setUnitOnBoard(0, 0, self.knightUnit)
+        calculatePossibleMoves(self.game, self.game.turn, True)
+        expectedMoves = ["21", "12"]
+        assertExpectedMovesResults(expectedMoves, self.knightUnit)
+        assertNumberOfWhiteAndTotalUnits(self.game, 1, 1)
+
+    def testUpperRightCornerKnightMoves(self):
+        self.game.setUnitOnBoard(7, 7, self.knightUnit)
+        calculatePossibleMoves(self.game, self.game.turn, True)
+        expectedMoves = ["56", "65"]
+        assertExpectedMovesResults(expectedMoves, self.knightUnit)
+        assertNumberOfWhiteAndTotalUnits(self.game, 1, 1)
+
+    def testLowerRightCornerKnightMoves(self):
+        self.game.setUnitOnBoard(7, 0, self.knightUnit)
+        calculatePossibleMoves(self.game, self.game.turn, True)
+        expectedMoves = ["51", "62"]
+        assertExpectedMovesResults(expectedMoves, self.knightUnit)
+        assertNumberOfWhiteAndTotalUnits(self.game, 1, 1)
+
+    def testUpperLeftPositionKnightMoves(self):
+        self.game.setUnitOnBoard(1, 6, self.knightUnit)
+        calculatePossibleMoves(self.game, self.game.turn, True)
+        expectedMoves = ["04", "24", "35", "37"]
+        assertExpectedMovesResults(expectedMoves, self.knightUnit)
+        assertNumberOfWhiteAndTotalUnits(self.game, 1, 1)
+
+    def testLowerLeftPositionKnightMoves(self):
+        self.game.setUnitOnBoard(1, 1, self.knightUnit)
+        calculatePossibleMoves(self.game, self.game.turn, True)
+        expectedMoves = ["03", "23", "32", "30"]
+        assertExpectedMovesResults(expectedMoves, self.knightUnit)
+        assertNumberOfWhiteAndTotalUnits(self.game, 1, 1)
+
+    def testUpperRightPositionKnightMoves(self):
+        self.game.setUnitOnBoard(6, 6, self.knightUnit)
+        calculatePossibleMoves(self.game, self.game.turn, True)
+        expectedMoves = ["74", "54", "45", "47"]
+        assertExpectedMovesResults(expectedMoves, self.knightUnit)
+        assertNumberOfWhiteAndTotalUnits(self.game, 1, 1)
+
+    def testLowerRightPositionKnightMoves(self):
+        self.game.setUnitOnBoard(6, 1, self.knightUnit)
+        calculatePossibleMoves(self.game, self.game.turn, True)
+        expectedMoves = ["73", "53", "42", "40"]
+        assertExpectedMovesResults(expectedMoves, self.knightUnit)
+        assertNumberOfWhiteAndTotalUnits(self.game, 1, 1)
 
     # --- OPPONENT UNITS ---
 
