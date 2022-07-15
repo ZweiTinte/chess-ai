a77762681027579e63790b398c14a2a725947b02
ZweiTinte <zweitinte@gmail.com>
2022-07-15 23:49:28 +0200

    print learning progress

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 # coding: utf-8
+from src.log import getAllDbFiles, logLearningProgress, printAllDbFiles
 from src.game import Game
 
 game = Game()
 game.start()
+
+# print the learning progress of the AI to the console
+#logLearningProgress()
--- src/log.py
+++ src/log.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 from src.database import loadData
-from src.databaseLocationString import generateDatabaseLocationString
+from src.databaseLocationString import DB_DIR_NAME, generateDatabaseLocationString
+import os
 
 LOG_FILE_NAME = "game_log.txt"
 
@@ -51,3 +52,46 @@ def logMoveableUnits(game):
     for u in data:
         units += u + ", "
     logToFile("moveable units: " + units[:-2])
+
+def printAllDbFiles():
+    for subdir, dirs, files in os.walk(DB_DIR_NAME):
+        for file in files:
+            print(os.path.join(subdir, file))
+
+def getAllDbFiles():
+    allFiles = []
+    for subdir, dirs, files in os.walk(DB_DIR_NAME):
+        for file in files:
+            allFiles.append(os.path.join(subdir, file))
+    return allFiles
+
+def getAllWinChances():
+    winChances = []
+    for file in getAllDbFiles():
+        data = loadData(file)
+        for unit in data:
+            for move in data[unit]:
+                chances = data[unit][move]
+                winChances.append(chances["w"] / (chances["w"] + chances["l"]))
+    return winChances
+
+def logLearningProgress():
+    winChances = getAllWinChances()
+    topTenAmount = 0
+    bottomTenAmount = 0
+    totalAmount = len(winChances)
+
+    for chance in winChances:
+        if chance >= 0.9:
+            topTenAmount += 1
+        elif chance <= 0.1:
+            bottomTenAmount += 1
+
+    topTenPercentage = round(100 *topTenAmount / totalAmount, 2)
+    bottomTenPercentage = round(100 * bottomTenAmount / totalAmount, 2)
+
+    print("Total amount of moves stored: " + str(totalAmount))
+    print("Amount of moves with win chance >= 90%: " + str(topTenAmount))
+    print("Amount of moves with win chance <= 10%: " + str(bottomTenAmount))
+    print("Percentage of moves with win chance >= 90%: " + str(topTenPercentage) + "%")
+    print("Percentage of moves with win chance <= 10%: " + str(bottomTenPercentage) + "%")
